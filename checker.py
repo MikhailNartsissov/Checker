dbc59d006ef407bd00914da665cbc868210894db
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2024-01-07 16:34:59 +0300

    0001-first-version-of-engine

---
+++ checker.py
@@ -0,0 +1,29 @@
+from config import (
+    CSV_FILE_PATH,
+    CSV_SEPARATOR,
+    BAD_RECORDS_FILE_PATH,
+    ERROR_STATISTICS_FILE_PATH,
+)
+
+incorrect_spc_code = 0
+no_switch_id = 0
+
+with open(CSV_FILE_PATH, "r", encoding="UTF-8") as csv:
+    for line in csv:
+        fields = line.split(CSV_SEPARATOR)
+
+        if not fields[0].isdigit():
+            with open(BAD_RECORDS_FILE_PATH, "a", encoding="UTF-8") as bad_records:
+                bad_records.write("Incorrect SPC Code: " + line)
+            incorrect_spc_code += 1
+
+        if fields[1] == "":
+            with open(BAD_RECORDS_FILE_PATH, "a", encoding="UTF-8") as bad_records:
+                bad_records.write("No switch ID specified: " + line)
+            no_switch_id += 1
+
+    with open(ERROR_STATISTICS_FILE_PATH, "w", encoding="UTF-8") as error_statistics:
+        error_statistics.write("incorrect_spc_code = " +
+                               str(incorrect_spc_code) +
+                               "\nno_switch_id = " +
+                               str(no_switch_id))
---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+Configuration parameters
+"""
+
+CSV_FILE_PATH = "./test.csv"
+CSV_SEPARATOR = ","
+BAD_RECORDS_FILE_PATH = "./bad_records.csv"
+ERROR_STATISTICS_FILE_PATH = "./error_statistics.txt"
