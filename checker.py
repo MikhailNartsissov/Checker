b6f660b74867eba5fc2125ca01cfa6264a772b3a
Mikhail Nartsissov <nartsissov_ma@mail.ru>
2024-01-07 16:57:31 +0300

    0002-encoding-added-and-filepath-removed

--- checker.py
+++ checker.py
@@ -1,6 +1,8 @@
 from config import (
     CSV_FILE_PATH,
     CSV_SEPARATOR,
+    CSV_ENCODING,
+    RES_ENCODING,
     BAD_RECORDS_FILE_PATH,
     ERROR_STATISTICS_FILE_PATH,
 )
@@ -8,21 +10,21 @@ from config import (
 incorrect_spc_code = 0
 no_switch_id = 0
 
-with open(CSV_FILE_PATH, "r", encoding="UTF-8") as csv:
+with open(CSV_FILE_PATH, "r", encoding=CSV_ENCODING) as csv:
     for line in csv:
         fields = line.split(CSV_SEPARATOR)
 
         if not fields[0].isdigit():
-            with open(BAD_RECORDS_FILE_PATH, "a", encoding="UTF-8") as bad_records:
+            with open(BAD_RECORDS_FILE_PATH, "a", encoding=RES_ENCODING) as bad_records:
                 bad_records.write("Incorrect SPC Code: " + line)
             incorrect_spc_code += 1
 
         if fields[1] == "":
-            with open(BAD_RECORDS_FILE_PATH, "a", encoding="UTF-8") as bad_records:
+            with open(BAD_RECORDS_FILE_PATH, "a", encoding=RES_ENCODING) as bad_records:
                 bad_records.write("No switch ID specified: " + line)
             no_switch_id += 1
 
-    with open(ERROR_STATISTICS_FILE_PATH, "w", encoding="UTF-8") as error_statistics:
+    with open(ERROR_STATISTICS_FILE_PATH, "w", encoding=RES_ENCODING) as error_statistics:
         error_statistics.write("incorrect_spc_code = " +
                                str(incorrect_spc_code) +
                                "\nno_switch_id = " +
--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 Configuration parameters
 """
 
-CSV_FILE_PATH = "./test.csv"
+CSV_FILE_PATH = "test.csv"
 CSV_SEPARATOR = ","
-BAD_RECORDS_FILE_PATH = "./bad_records.csv"
-ERROR_STATISTICS_FILE_PATH = "./error_statistics.txt"
+CSV_ENCODING = "UTF-8"
+RES_ENCODING = "UTF-8"
+BAD_RECORDS_FILE_PATH = "bad_records.csv"
+ERROR_STATISTICS_FILE_PATH = "error_statistics.txt"
